88f30516278c4ba3db2c78037b9aaf7a43a7b157
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-02-03 22:47:51 +0100

    Add useless scripts and Input System

---
+++ Assets/Scripts/PlayerBattleController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerBattleController : BaseController
+{
+    protected override Vector2 GetMoveDir()
+    {
+        return new (5.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerFreeController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerFreeController : BaseController
+{
+     protected override Vector2 GetMoveDir()
+     {
+          var x = Input.GetAxisRaw("Horizontal");
+          var y = Input.GetAxisRaw("Vertical");
+          
+          return new (x, y);
+     }
+}
